b4061bba2df0b12313de44a6394569acc7e60e27
Takashi Tsuchiya <takashi_tsuchiya@relationsgroup.co.jp>
2015-01-14 07:57:24 +0900

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(function(){
-    $('a[href^=#]').click(function(){
+  $('.index-navigation').click(function(){
       var speed = 500;
       var href= $(this).attr("href");
-      var target = $(href == "#" || href == "" ? 'html' : href);
+      var target = $(href == "#" || href == "" ? 'html' : href.replace(/(.*)#/, "#"));
       var position = target.offset().top - 64;
       $("html, body").animate({scrollTop:position}, speed, "swing");
       return false;
